dec167a26022d486d1f99c631cfa2c8d5e13227b
yadavn01 <ynaman29@gmail.com>
2024-06-25 10:42:55 +1200

    register and login models added

---
+++ backend/models/LoginModel.cs
@@ -0,0 +1,5 @@
+public class LoginModel 
+{
+    public string email { get; set; }
+    public string password { get; set; }
+}
---
+++ backend/models/RegisterModel.cs
@@ -0,0 +1,5 @@
+public class RegisterModel 
+{
+    public string email { get; set; }
+    public string password { get; set; }
+}
