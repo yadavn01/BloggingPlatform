a76d66fcc85531f7ba3eaba06b0f0de5848ac17b
yadavn01 <ynaman29@gmail.com>
2024-06-28 18:12:15 +1200

    bump cache and reserving jwt wip

--- backend/obj/Debug/net8.0/BloggingPlatform.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/BloggingPlatform.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BloggingPlatform")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac33454255ca2685864679d7c70216d5fa30a510")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c51e4952f8239fc8de693b965b8ff0d12aaf43ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("BloggingPlatform")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BloggingPlatform")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- frontend/src/components/Register.tsx
+++ frontend/src/components/Register.tsx
@@ -3,10 +3,9 @@ import { register } from '../authService';
 import { useNavigate } from "react-router-dom";
 
 const Register = () => {
-    const[email, setEmail] = useState('');
-    const[password, setPassword] = useState('');
-    const[message, setMessage] = useState('');
-    const [errorMessage, setErrorMessage] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [message, setMessage] = useState('');
     const navigate = useNavigate();
 
     const handleClear = () => {
@@ -16,17 +15,17 @@ const Register = () => {
     }
 
 
-const handleRegister = async (e: React.FormEvent)  => {
-e.preventDefault();
-try {
-    await register({email,password});
-    setMessage("Registration Successfull")
-    navigate('/login')
-}
-catch(error) {
-    console.log(error);
-    setMessage("Registration not complete, try again!")
-}
+    const handleRegister = async (e: React.FormEvent) => {
+        e.preventDefault();
+        try {
+            await register({ email, password });
+            setMessage("Registration Successfull")
+            navigate('/login')
+        }
+        catch (error) {
+            console.log(error);
+            setMessage("Registration not complete, try again!")
+        }
 
 }
     return (
