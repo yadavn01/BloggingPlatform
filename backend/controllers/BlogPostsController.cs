5ba2f34c31ab80987bf97211bf5ba03eea1e9aba
yadavn01 <ynaman29@gmail.com>
2024-07-05 14:48:21 +1200

    Blogposts controller added

---
+++ backend/controllers/BlogPostsController.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Threading.Tasks;
+
+[Route("api/[controller]")]
+[ApiController]
+
+public class BlogPostsController : ControllerBase
+{
+     private readonly ApplicationDbContext _context;
+
+     public BlogPostsController(ApplicationDbContext context)
+     {
+        _context = context;
+     }
+
+     [HttpGet]
+     public async Task<IActionResult> GetBlogPosts()
+     {
+        var blogposts = _context.BlogPosts.ToListAsync();
+        return Ok(blogposts);
+     }
+     [HttpPost]
+     public async Task<IActionResult> CreateBlogPost([FromBody] BlogPost blogPost)
+     {
+        blogPost.CreatedAt = DateTime.UtcNow;
+        _context.BlogPosts.Add(blogPost);
+        await _context.SaveChangesAsync();
+        return CreatedAtAction(nameof(GetBlogPosts), new { id = blogPost.Id }, blogPost);
+     }
+}
